80dbeb567e8a2cb85d900a1a674fb72e37a4a21c
Leandro Pacheco <pacheco@shadow-studio.(none)>
2013-04-01 15:09:09 +0200

    removing unused stuff

--- lps-defuns.el
+++ lps-defuns.el
@@ -9,7 +9,6 @@
 (defun toggle-maximize ()
   "Maximize (or unmaximize) window"
   (interactive)
-  ;; TODO: this only works for X. patches welcome for other OSes.
   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
                          '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
