4d78163120be5eade57cb2661c3a51583c20d2f9
pacheco <le.pacheco@gmail.com>
2014-02-17 11:57:41 +0100

    f12 to toggle-fullscreen

--- lps-bindings.el
+++ lps-bindings.el
@@ -19,7 +19,7 @@
 (global-set-key (kbd "C-<") (lambda () (interactive) (other-window -1)))
 (global-set-key (kbd "C->") 'other-window)
 (global-set-key (kbd "C-x C-d") 'dired)
-(global-set-key (kbd "<f12>") 'toggle-maximize)
+(global-set-key (kbd "<f12>") 'toggle-fullscreen)
 (global-set-key (kbd "<f10>") 'lps/shell-other-window)
 (global-set-key (kbd "<f5>") 'first-error)
 (global-set-key (kbd "<f6>") 'recompile)
