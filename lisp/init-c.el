8eb3d7a2324546531bd9702af992e39171761b69
pacheco <le.pacheco@gmail.com>
2015-03-18 16:22:03 +0100

    c stuff - mainly ggtags

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -1,3 +1,13 @@
+(require-package 'ggtags)
+
 (setq-default c-basic-offset 4)
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (ggtags-mode 1)
+            (local-set-key (kbd "C-h") 'c-electric-backspace)
+            (setq c-auto-newline nil)
+            (c-toggle-hungry-state 1)
+            (c-set-offset 'statement-case-open 0)))
 
 (provide 'init-c)
