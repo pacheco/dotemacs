4a22e52f9cb9e869c61cbcc890f6be0370777486
pacheco <le.pacheco@gmail.com>
2015-03-19 20:40:01 +0100

    didnt like c-toggle-hungry-state... disabling

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -33,7 +33,7 @@ error (e.g. cursor not pointing a tag), ask for a symbol name"
             (define-key semantic-mode-map (kbd "C-c C-d") 'semantic-ia-show-summary)
             (local-set-key (kbd "C-h") 'c-electric-backspace)
             (setq c-auto-newline nil)
-            (c-toggle-hungry-state 1)
+            ;(c-toggle-hungry-state 1)
             (c-set-offset 'statement-case-open 0)))
 
 (provide 'init-c)
