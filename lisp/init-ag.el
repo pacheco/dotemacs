4b95e898af2ede49d45b2e3c004142cb6f05d36c
pacheco <le.pacheco@gmail.com>
2015-03-24 15:32:24 +0100

    <C-?> does ag-regexp and <C-M-?> does ag-project

--- lisp/init-ag.el
+++ lisp/init-ag.el
@@ -1,6 +1,7 @@
 (when (executable-find "ag") ; install silversearcher-ag
   (require-package 'ag)
   (setq-default ag-highlight-search t)
-  (global-set-key (kbd "C-?") 'ag-project))
+  (global-set-key (kbd "C-?") 'ag-regexp)
+  (global-set-key (kbd "C-M-?") 'ag-project))
 
 (provide 'init-ag)
