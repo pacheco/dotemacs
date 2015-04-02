1a959efcab073c471dba290aa59449d41192353c
pacheco <le.pacheco@gmail.com>
2015-04-02 14:44:01 +0200

    do not indent c++ namespaces

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -1,7 +1,13 @@
 (require-package 'ggtags)
 (require-package 'cedet)
 
+;;; basic identation options
 (setq-default c-basic-offset 4)
+
+;;; c++ indentation options
+(c-add-style "my-c++-style" '("cc-mode"
+                             (c-offsets-alist . ((innamespace . [0]))) ; do not indent namespaces
+                             ))
 
 ;;; GDB config
 (setq-default gdb-many-windows 1)
@@ -14,7 +20,6 @@
 (global-semanticdb-minor-mode 1)
 (global-semantic-idle-scheduler-mode 1)
 
-
 (defun lps/semantic-jump ()
   "Try to jump to the tag pointed by the cursor. If there is an
 error (e.g. cursor not pointing a tag), ask for a symbol name"
@@ -23,6 +28,7 @@ error (e.g. cursor not pointing a tag), ask for a symbol name"
       (semantic-ia-fast-jump nil)
     (error (semantic-complete-jump))))
 
+;;; C/C++ hooks
 (add-hook 'c-mode-common-hook
           (lambda ()
             (setq company-backends (delete 'company-clang company-backends))
@@ -35,5 +41,10 @@ error (e.g. cursor not pointing a tag), ask for a symbol name"
             (setq c-auto-newline nil)
             ;(c-toggle-hungry-state 1)
             (c-set-offset 'statement-case-open 0)))
+
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (c-set-style "my-c++-style")))
+
 
 (provide 'init-c)
