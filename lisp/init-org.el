c00d0d11c4e4c67bdac52675ac536473f0495d5c
Leandro Pacheco <contact@leandropacheco.com>
2015-12-11 11:44:55 +0100

    org agenda files should be a local config

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,5 +1,3 @@
 (setq org-log-done 'time
-      org-agenda-window-setup 'current-window
-      org-agenda-files (list "~/Dropbox/org/TODO.org"))
-
+      org-agenda-window-setup 'current-window)
 (provide 'init-org)
