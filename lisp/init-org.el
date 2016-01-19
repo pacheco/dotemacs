43abbd4d65f795df601e205e32e4e72347f3132d
Leandro Pacheco <contact@leandropacheco.com>
2016-01-19 10:46:31 +0100

    evince as default pdf reader for org-mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,3 +1,9 @@
 (setq org-log-done 'time
       org-agenda-window-setup 'current-window)
+
+;;; evince as default pdf reader
+(when (eq system-type 'gnu/linux)
+  (add-to-list 'org-file-apps
+               '("\\.pdf\\'" . "evince %s")))
+
 (provide 'init-org)
