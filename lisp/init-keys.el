c113d8b371e5e5e73f8ae1d73fc4d9b98bdac51b
Leandro Pacheco <contact@leandropacheco.com>
2016-01-19 10:46:08 +0100

    C-= increases text size

--- lisp/init-keys.el
+++ lisp/init-keys.el
@@ -27,6 +27,7 @@
 (global-set-key (kbd "M-Z") 'zap-up-to-char)
 (global-set-key (kbd "<f8>") 'org-agenda)
 (global-set-key (kbd "<f9>") 'org-cycle-agenda-files)
+(global-set-key (kbd "C-=") 'text-scale-increase)
 
 
 (provide 'init-keys)
