631c1572077c5316b15a0695b7e4dc9f27d677c8
Leandro Pacheco <contact@leandropacheco.com>
2016-01-22 13:10:36 +0100

    fix to org mode config

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,9 +1,10 @@
 (setq org-log-done 'time
       org-agenda-window-setup 'current-window)
 
-;;; evince as default pdf reader
-(when (eq system-type 'gnu/linux)
-  (add-to-list 'org-file-apps
-               '("\\.pdf\\'" . "evince %s")))
+(add-hook 'org-mode-hook
+          (lambda ()
+            ;; evince as default external pdf reader
+            (when (eq system-type 'gnu/linux)
+              (add-to-list 'org-file-apps '("\\.pdf\\'" . "evince %s")))))
 
 (provide 'init-org)
