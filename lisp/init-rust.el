2230fc8115c3b3c2eb6bdfeb3ca56f0334817e6c
Leandro Pacheco <contact@leandropacheco.com>
2016-02-23 16:32:32 +0100

    Rust auto-complete with racer

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,3 +1,11 @@
 (require-package 'rust-mode)
+(require-package 'racer)
+
+(setq racer-cmd (expand-file-name "~/.cargo/bin/racer"))
+(setq racer-rust-src-path (expand-file-name "~/usr/rustc-1.6.0/src"))
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
 
 (provide 'init-rust)
