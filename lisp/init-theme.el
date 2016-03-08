f887270e4c3c2187bbd4b811679e12e83a52ca5d
Leandro Pacheco <contact@leandropacheco.com>
2016-03-08 17:31:18 +0100

    change theme to zenburn

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,7 +1,8 @@
 ;; (require-package 'color-theme-sanityinc-solarized)
-;; (require-package 'color-theme-sanityinc-tomorrow)
 
-(load-theme 'manoj-dark t)
+(require-package 'zenburn)
+(load-theme 'zenburn t)
+
 (set-default-font "Monospace-10")
 
 (provide 'init-theme)
