af05a63e766396fbb1ffe32b8b9af626181a8da7
Leandro Pacheco <contact@leandropacheco.com>
2016-03-18 12:01:49 +0100

    erlang distel setup

--- lisp/init-erlang.el
+++ lisp/init-erlang.el
@@ -1,13 +1,25 @@
+(push "~/.emacs.d/site-lisp/distel/elisp/" load-path)
 (require-package 'erlang)
+(require 'company-distel)
 (require 'erlang-start)
 
-;;; TODO: check distel
+;;; distel
+(require 'distel)
+(distel-setup)
+(setq inferior-erlang-prompt-timeout t)
+(setq inferior-erlang-machine-options '("-sname" "emacs"))
+(setq erl-nodename-cache
+      (make-symbol
+       (concat
+        "emacs@"
+        (car (split-string (shell-command-to-string "hostname"))))))
+
 (mapcar (lambda (x) (add-to-list 'auto-mode-alist (cons x 'erlang-mode)))
         '("relx.config" "app.config"))
 
 (add-hook 'erlang-mode-hook
           (lambda ()
-            (define-key erlang-mode-map (kbd "C-c h") 'erlang-man-function)))
-
+            (define-key erlang-mode-map (kbd "C-c h") 'erlang-man-function)
+            (setq company-backends '(company-distel))))
 
 (provide 'init-erlang)
