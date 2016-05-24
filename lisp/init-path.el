17bde016dab37f2cf175d9794adfa832bfe387db
Leandro Pacheco <contact@leandropacheco.com>
2016-05-24 15:10:16 +0200

    env variable initialization

--- lisp/init-path.el
+++ lisp/init-path.el
@@ -2,7 +2,15 @@
 
 (require 'exec-path-from-shell)
 
-(dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
+(dolist (var '("SSH_AUTH_SOCK"
+               "SSH_AGENT_PID"
+               "GPG_AGENT_INFO"
+               "LANG"
+               "LC_CTYPE"
+               "LD_LIBRARY_PATH"
+               "LD_LIBRARY_PATH"
+               "C_INCLUDE_PATH"
+               "CPLUS_INCLUDE_PATH"))
   (add-to-list 'exec-path-from-shell-variables var))
 
 (exec-path-from-shell-initialize)
