7ef72dd7930933ed29761a64fd8c355fdc8b6cba
Leandro Pacheco <contact@leandropacheco.com>
2016-10-24 10:31:51 +0200

    rust mode

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,11 +1,6 @@
 (require-package 'rust-mode)
 (require-package 'racer)
 
-(setq racer-cmd (expand-file-name "~/.cargo/bin/racer"))
-
-(setq racer-rust-src-path (expand-file-name "~/usr/rust-stable/src"))
-;; (setq racer-rust-src-path (expand-file-name "~/usr/rust-git/src"))
-
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
