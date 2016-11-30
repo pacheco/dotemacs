60e0a88b7729a4243b3d942048025451a8ba5c6f
Leandro Pacheco <contact@leandropacheco.com>
2016-11-30 11:10:19 +0100

    rust racer setup

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,8 +1,12 @@
 (require-package 'rust-mode)
 (require-package 'racer)
+(require-package 'flycheck-rust)
 
 (add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'rust-mode-hook #'flycheck-mode)
+(add-hook 'rust-mode-hook #'electric-pair-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
 (provide 'init-rust)
