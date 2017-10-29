5e5d4a75f7d9b5c1697479503e096322f33ce0f4
Leandro Pacheco <contact@leandropacheco.com>
2017-10-29 21:50:07 +0100

    rust mode

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,12 +1,24 @@
 (require-package 'rust-mode)
+
+;;; racer-mode config
+;;; ---------------------------
 (require-package 'racer)
-(require-package 'flycheck-rust)
 
 (add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'rust-mode-hook #'flycheck-mode)
 (add-hook 'rust-mode-hook #'electric-pair-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+
+(add-hook 'racer-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c C-t") 'racer-describe)))
+
+
+;; (require-package 'flycheck-rust)
+;; (add-hook 'rust-mode-hook #'flycheck-mode)
+;; (add-hook 'racer-mode-hook #'eldoc-mode)
+;; (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+
+;; TODO: try using lsp
+;; ------------------------
 
 (provide 'init-rust)
